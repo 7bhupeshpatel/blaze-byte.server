e6b001c64d04ef445463aeab91a280ffd60d0f94
bhupesh-sarvarth <bhupesh.patel@sarvarth.com>
2026-02-19 00:46:55 +0530

    db config file, removed adapter

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+// import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
@@ -17,12 +17,12 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL 
 });
 
-const adapter = new PrismaPg(pool);
+// const adapter = new PrismaPg(pool);
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
+    
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
